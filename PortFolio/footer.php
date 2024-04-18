b7040f78217520512fce1953227b40838a0b71c6
AlexandreGoussu <131788089+AlexandreGoussu@users.noreply.github.com>
2024-04-18 12:04:17 +0200

    Add files via upload

---
+++ PortFolio/footer.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<footer>
+    <div class="container">
+        <div class="grid">
+            <div class="grid__item">
+                <p>GOUSSU Alexandre</p>
+            </div>
+            <div class="grid__item">
+                <p>Copyright © 2023. All Rights Reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
